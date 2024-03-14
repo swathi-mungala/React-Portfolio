4f6e34521a206d439ac72ebd6d2c97ff22be8a73
swathi-mungala <mswathi584@gmail.com>
2024-03-14 17:41:05 +0000

    Changed folder structure and added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
 import './App.css'
-
+import Header from './components/Header/Header'
 
 function App() {
   
 
   return (
     <>
-      Hello!
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from './../Navbar/Navbar';
+
+function Header() {
+  return (
+    <Navbar />
+  );
+}
+
+export default Header;
